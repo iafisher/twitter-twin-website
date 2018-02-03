a2ef91efdb3c4e6f030c5fbb5a5d6f87ac047af1
Ian Fisher <ianfisher45@gmail.com>
2018-02-03 12:11:57 -0500

    Insert percentage-match under each person's image

--- classifier_app/static/classifier_app/js/index.js
+++ classifier_app/static/classifier_app/js/index.js
@@ -1,5 +1,37 @@
 $(document).ready(function () {
     $("#twitter-input-button").click(function () {
-        console.log("Clicked!");
+        var url = '/ajax_classify/' + $("#twitter-input").val();
+        $.get(url, function (data) {
+            var coefficients = data['coefficients'];
+            for (var i = 0; i < coefficients.length; i++) {
+                var elem = makeCoefficientElement(coefficients, i);
+                $("#img-row div:nth-child(" + (i+1) + ")").append(elem);
+            }
+        });
+
     });
 });
+
+
+function makeCoefficientElement(coefficients, i) {
+    var perc = ("" + coefficients[i] * 100) + "%";
+    var classes = ["coefficient"];
+    if (isMax(coefficients, i)) {
+        classes.push("coefficient-green");
+    } else if (coefficients[i] > 0.1) {
+        classes.push("coefficient-yellow");
+    } else {
+        classes.push("coefficient-red");
+    }
+    return "<p class=\"" + classes.join(" ") + "\">" + perc + "</p>";
+}
+
+
+function isMax(coefficients, i) {
+    for (var j = 0; j < coefficients.length; j++) {
+        if (coefficients[j] > coefficients[i]) {
+            return false;
+        }
+    }
+    return true;
+}
